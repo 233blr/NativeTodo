0f3852dc0c5f0ec851f02ede7bb92edcd87ed8c6
Vadim <233blr@gmail.com>
2021-12-28 02:29:49 +0300

    Added AddTodo component

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddToto';
 
 export default function App() {
   const [todos, setTodos] = useState([
     { id: 0, importance: 0, text: 'Bue coffe', date: '11 Dec 2021 15:05', status: true, },
     { id: 1, importance: 2, text: 'Learn JS', date: '16 Dec 2021 16:00', status: false, },
-    { id: 2, importance: 3, text: 'Call someone', date: '25 Dec 2021 19:05', status: true, },
-    { id: 3, importance: 3, text: 'Buy milk', date: '30 Dec 2021 20:45', status: true, },
+    { id: 2, importance: 3, text: 'Buy milk', date: '30 Dec 2021 20:45', status: true, },
+    { id: 3, importance: 3, text: 'Buy tea', date: '30 Dec 2021 20:45', status: true, },
+    { id: 4, importance: 3, text: 'Buy bye', date: '30 Dec 2021 20:45', status: true, },
   ]);
 
   const deleteHandler = (id) => {
@@ -18,7 +20,24 @@ export default function App() {
     )));
   };
 
-  // let date = new Date().toUTCString().slice(5, -7);
+  const addTodoHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return ([
+          {
+            id: prevTodos.length + 1,
+            importance: 0,
+            text: text,
+            date: new Date().toUTCString().slice(5, -7),
+            status: false,
+          },
+          ...prevTodos,
+        ]);
+      });
+    } else {
+      Alert.alert('Oops, the text must be longer!');
+    }
+  }
 
   // colors
   // #FCFCFC
@@ -32,12 +51,13 @@ export default function App() {
       <View>
         <Header />
         <View style={styles.content}>
-          {/* To form */}
+          <AddTodo addTodoHandler={addTodoHandler} />
           <View style={styles.list}>
             <FlatList
               data={todos}
               renderItem={({ item }) => (
                 <TodoItem
+                  key={item.id}
                   props={item}
                   pressHandler={deleteHandler}
                 />
@@ -55,9 +75,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FCFCFC',
   },
-  content: {
-    // padding: 40,
-  },
+  // content: {},
   list: {
     marginTop: 30,
   },
---
+++ components/AddToto.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Button, View } from 'react-native';
+
+export default function AddTodo({ addTodoHandler }) {
+  const [text, setText] = useState('')
+
+  const chengeHandler = (value) => {
+    setText(value);
+  }
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder='new text...'
+        onChangeText={chengeHandler}
+      />
+      <Button
+        title='ADD TODO'
+        color='#2C2C2C'
+        onPress={() => addTodoHandler(text)}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 10,
+    margin: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#2C2C2C',
+  },
+})
