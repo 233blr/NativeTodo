44efaf3a23e97af406b15edf059772d4885481c8
Vadim <233blr@gmail.com>
2021-12-27 21:50:47 +0300

    Added main markup

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodo] = useState([
+    { id: 0, rating: 0, text: 'Bue coffe', date: '11 Dec 2021 15:05', status: true, },
+    { id: 1, rating: 2, text: 'Learn JS', date: '16 Dec 2021 16:00', status: false, },
+    { id: 2, rating: 5, text: 'Call someone', date: '25 Dec 2021 19:05', status: true, },
+  ]);
+
+  // let date = new Date().toUTCString().slice(5, -7);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        {/* Header */}
+        <View style={styles.content}>
+          {/* To form */}
+          <View style={styles.list}>
+            {/*  */}
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (<Text>{item.text}</Text>)}
+            />
+          </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +35,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    margin: 20,
   },
 });
